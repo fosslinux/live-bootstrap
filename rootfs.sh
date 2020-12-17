19b41d22f4a0be824aef34658ede8b40657aa2f8
fosslinux <fosslinux@aussies.space>
2020-12-17 11:25:36 +1100

    Move everything into sysa

--- rootfs.sh
+++ rootfs.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 set -ex
 
+pushd sysa
+
+# SYSTEM A
+
 # Setup tmp 
 mkdir -p tmp/
 sudo mount -t tmpfs -o size=8G tmpfs tmp
 
-# Now copy everything over
-
 # base: mescc-tools-seed
 cp -r mescc-tools-seed/x86/* tmp
 cp -r mescc-tools-seed/{M2-Planet,mes-m2,mescc-tools} tmp/
-cp bootstrap-seeds/POSIX/x86/kaem-optional-seed tmp/init
-cp bootstrap-seeds/POSIX/x86/kaem-optional-seed tmp/
-cp -r bootstrap-seeds tmp/
+cp ../bootstrap-seeds/POSIX/x86/kaem-optional-seed tmp/init
+cp ../bootstrap-seeds/POSIX/x86/kaem-optional-seed tmp/
+cp -r ../bootstrap-seeds tmp/
 mkdir tmp/bin
 
 # blynn-compiler
@@ -35,7 +37,9 @@ qemu-system-x86_64 -enable-kvm \
     -m 16G \
     -nographic \
     -no-reboot \
-    -kernel ../kernel -initrd initramfs.igz -append console=ttyS0,kernel.panic=2
+    -kernel ../../kernel -initrd initramfs.igz -append console=ttyS0,kernel.panic=2
 
 # Cleanup
 sudo umount tmp
+
+popd
