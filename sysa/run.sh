756ba1702b54380423172a0f6f7d31a924012f13
Paul Dersey <pdersey@gmail.com>
2021-02-19 11:14:54 -0500

    Rebuild musl libc after building tcc-musl, and then rebuild tcc-musl
    This fixes an issue with printf not printing doubles or floats
    correctly, and probably other subtle issues.
    
    tcc-musl also uses floats extensively, so rebuild it as well, to fix
    some other potential issues.
    
    Update checksums for all resulting binaries which have now changed.
    
    Co-authored-by: fosslinux <fosslinux@aussies.space>

--- sysa/run.sh
+++ sysa/run.sh
@@ -2,6 +2,7 @@
 
 # SPDX-FileCopyrightText: 2021 Andrius Štikonas <andrius@stikonas.eu>
 # SPDX-FileCopyrightText: 2021 fosslinux <fosslinux@aussies.space>
+# SPDX-FileCopyrightText: 2021 Paul Dersey <pdersey@gmail.com>
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
 
@@ -15,41 +16,47 @@ export PREFIX=/after
 build flex-2.5.11
 
 # Part 21
-build musl-1.1.24
+build musl-1.1.24 musl-1.1.24.sh checksums/pass1
 
 # Part 22
-build tcc-0.9.27 tcc-musl.sh checksums/tcc-musl
+build tcc-0.9.27 tcc-musl-pass1.sh checksums/tcc-musl-pass1
 
 # Part 23
-build m4-1.4.7
+build musl-1.1.24 musl-1.1.24.sh checksums/pass2
 
 # Part 24
-build flex-2.6.4
+build tcc-0.9.27 tcc-musl-pass2.sh checksums/tcc-musl-pass2
 
 # Part 25
+build m4-1.4.7
+
+# Part 26
+build flex-2.6.4
+
+# Part 27
 build bison-3.4.1 stage1.sh checksums/stage1
 build bison-3.4.1 stage2.sh checksums/stage2
 build bison-3.4.1 stage3.sh checksums/stage3
 
-# Part 26
+# Part 28
 build grep-2.4
 
-# Part 27
+# Part 29
 build diffutils-2.7
 
-# Part 28
+# Part 30
 build coreutils-5.0 coreutils-5.0.sh checksums/pass2
 
-# Part 29
+# Part 31
 build gawk-3.0.4
 
-# Part 30
+# Part 32
 build perl-5.000
 
-# Part 31
+# Part 33
 build perl-5.003
 
-# Part 32
+# Part 34
 build perl5.004_05
 
 echo "Bootstrapping completed."
