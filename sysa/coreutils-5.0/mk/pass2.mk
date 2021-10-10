35a7df58b7084eb2dd1b634f3cb4f583056b0fdb
Andrius Štikonas <andrius@stikonas.eu>
2021-10-10 12:18:08 +0100

    Build chroot in coreutils-5.0 step.

--- sysa/coreutils-5.0/mk/pass2.mk
+++ sysa/coreutils-5.0/mk/pass2.mk
@@ -83,7 +83,7 @@ CFLAGS  = -I . -I lib \
 
 SRC_DIR=src
 
-COREUTILS = basename cat chmod cksum comm csplit cut dd dirname echo env expand expr factor false fmt fold head id join kill link ln logname mkfifo mkdir mknod nl od paste pathchk printf ptx pwd readlink rmdir seq sleep sort split sum sync tail tee tr tsort uname unexpand uniq unlink wc whoami test true yes
+COREUTILS = basename cat chmod chroot cksum comm csplit cut dd dirname echo env expand expr factor false fmt fold head id join kill link ln logname mkfifo mkdir mknod nl od paste pathchk printf ptx pwd readlink rmdir seq sleep sort split sum sync tail tee tr tsort uname unexpand uniq unlink wc whoami test true yes
 
 BINARIES = $(addprefix $(SRC_DIR)/, $(COREUTILS))
 
