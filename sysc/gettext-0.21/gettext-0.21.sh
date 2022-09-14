9d84539a70e5140bb1519230e6c3d6836f3dcd4a
Dor Askayo <dor.askayo@gmail.com>
2022-09-14 18:32:09 +0300

    Set --libdir when configuring gettext 0.21

--- sysc/gettext-0.21/gettext-0.21.sh
+++ sysc/gettext-0.21/gettext-0.21.sh
@@ -20,7 +20,7 @@ src_prepare() {
 }
 
 src_configure() {
-    ./configure --prefix="${PREFIX}" --enable-static --disable-shared --disable-java
+    ./configure --prefix="${PREFIX}" --libdir="${PREFIX}/lib/musl" --enable-static --disable-shared --disable-java
 }
 
 src_compile() {
