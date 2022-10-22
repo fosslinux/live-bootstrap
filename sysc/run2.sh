2325df7f38ff39512c0ee6b0bd2a342f788eddfc
fosslinux <fosslinux@aussies.space>
2022-10-23 10:18:32 +1100

    Add Python-3.1.5

--- sysc/run2.sh
+++ sysc/run2.sh
@@ -113,6 +113,10 @@ build Python-2.3.7 stage2.sh
 
 build Python-2.5.6
 
+build Python-3.1.5 stage1.sh
+
+build Python-3.1.5 stage2.sh
+
 if [ "$FORCE_TIMESTAMPS" = True ] ; then
     echo 'Forcing all files timestamps to be 0 unix time.'
     canonicalise_all_files_timestamp
