eb607794cd01eadc6812b245dd296a8539fba1a3
fosslinux <fosslinux@aussies.space>
2023-02-02 21:45:12 +1100

    Clean up TCC buildcode
    It was unstandard & some files were not correctly in DESTDIR

--- sysa/tcc-0.9.27/tcc-musl-pass1.sh
+++ sysa/tcc-0.9.27/tcc-musl-pass1.sh
@@ -1,5 +1,5 @@
 # SPDX-FileCopyrightText: 2021-2022 Andrius Štikonas <andrius@stikonas.eu>
-# SPDX-FileCopyrightText: 2022 fosslinux <fosslinux@aussies.space>
+# SPDX-FileCopyrightText: 2022-23 fosslinux <fosslinux@aussies.space>
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
 
@@ -10,42 +10,38 @@ src_prepare() {
 }
 
 src_compile() {
-    export libdir=${LIBDIR}
-    export incdir=${PREFIX}/include/musl
-    export bindir=${PREFIX}/bin
-
-    mkdir -p "${libdir}/tcc"
-
     # We first have to recompile using tcc-0.9.26 as tcc-0.9.27 is not self-hosting,
     # but when linked with musl it is.
+    ln -sf "${PREFIX}/lib/mes/tcc/libtcc1.a" ./libtcc1.a
+
     for TCC in tcc-0.9.26 ./tcc-musl; do
-        ${TCC} \
+        "${TCC}" \
             -v \
             -static \
             -o tcc-musl \
             -D TCC_TARGET_I386=1 \
-            -D CONFIG_TCCDIR=\""${libdir}/tcc"\" \
-            -D CONFIG_TCC_CRTPREFIX=\""${libdir}"\" \
+            -D CONFIG_TCCDIR=\""${LIBDIR}/tcc"\" \
+            -D CONFIG_TCC_CRTPREFIX=\""${LIBDIR}"\" \
             -D CONFIG_TCC_ELFINTERP=\"/musl/loader\" \
-            -D CONFIG_TCC_LIBPATHS=\""${libdir}:${libdir}/tcc"\" \
-            -D CONFIG_TCC_SYSINCLUDEPATHS=\""${incdir}"\" \
-            -D TCC_LIBGCC=\""${libdir}/libc.a"\" \
+            -D CONFIG_TCC_LIBPATHS=\""${LIBDIR}:${LIBDIR}/tcc"\" \
+            -D CONFIG_TCC_SYSINCLUDEPATHS=\""${PREFIX}/include/musl"\" \
+            -D TCC_LIBGCC=\""${LIBDIR}/libc.a"\" \
             -D CONFIG_TCC_STATIC=1 \
             -D CONFIG_USE_LIBGCC=1 \
             -D TCC_VERSION=\"0.9.27\" \
             -D ONE_SOURCE=1 \
+            -B . \
             tcc.c
 
         # libtcc1.a
+        rm -f libtcc1.a
         ${TCC} -c -D HAVE_CONFIG_H=1 lib/libtcc1.c
-        ${TCC} -ar cr "${libdir}/tcc/libtcc1.a" libtcc1.o
+        ${TCC} -ar cr libtcc1.a libtcc1.o
     done
 }
 
 src_install() {
-    # Remove old tcc binaries
-    rm "${bindir}/tcc"
-    rm "${bindir}"/boot*-tcc "${bindir}/tcc-0.9.26" "${bindir}/mes-tcc"
-    install -D tcc-musl "${DESTDIR}${bindir}/tcc-musl"
-    ln -s tcc-musl "${DESTDIR}${bindir}/tcc"
+    install -D tcc-musl "${DESTDIR}${PREFIX}/bin/tcc-musl"
+    ln -s tcc-musl "${DESTDIR}${PREFIX}/bin/tcc"
+    install -D libtcc1.a "${DESTDIR}${LIBDIR}/tcc/libtcc1.a"
 }
