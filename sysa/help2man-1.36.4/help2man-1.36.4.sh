71d82c783bdbff1c145494b07a9ac83f71ead321
rick-masters <grick23@gmail.com>
2023-02-08 19:43:49 +0000

    Set perl config vars needed to create help2man shebang.

--- sysa/help2man-1.36.4/help2man-1.36.4.sh
+++ sysa/help2man-1.36.4/help2man-1.36.4.sh
@@ -18,13 +18,6 @@ src_configure() {
 
 src_compile() {
     make MAKEINFO=true
-
-    # fix a broken shebang
-    chmod +w help2man
-    tail -n +6 help2man > help2man.tmp
-    echo "#!${PREFIX}/bin/perl" > help2man
-    cat help2man.tmp >> help2man
-    rm help2man.tmp
 }
 
 src_install() {
