1bffe441541635dc0629009bd2a26c537dd9ee45
rick-masters <rick-masters@users.noreply.github.com>
2024-01-07 20:37:15 -0500

    Upgrade Fiwix to 1.5.0-lb1, pulling from upstream Mikaku repo. (#397)

---
+++ steps/fiwix-1.5.0-lb1/files/custom_limits.h
@@ -0,0 +1,8 @@
+/* SPDX-FileCopyrightText: 2024 Richard Masters <grick23@gmail.com> */
+/* SPDX-License-Identifier: MIT */
+#undef CHILD_MAX
+#define CHILD_MAX	4096
+#undef OPEN_MAX
+#define OPEN_MAX	1536
+#undef FD_SETSIZE
+#define FD_SETSIZE	OPEN_MAX 
