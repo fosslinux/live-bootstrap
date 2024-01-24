7f967fe2dcd3adc5367759becbb363f0497e09d3
fosslinux <fosslinux@aussies.space>
2024-01-24 14:45:07 +1100

    Cleanup the filesystem at end of bootstrap

---
+++ steps/improve/cleanup_filesystem.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# SPDX-FileCopyrightText: 2024 fosslinux <fosslinux@aussies.space>
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+rm -rf /external/repo-preseeded
+rm -rf /steps/*/build
+rm -rf /steps/*/src
