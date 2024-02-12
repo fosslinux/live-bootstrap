4e0a0f03dd18b88a12cc511655ccb71ef4d90723
Gábor Stefanik <netrolller.3d@gmail.com>
2024-02-12 13:35:49 +0100

    Capture everything built before the repo system in a base tarball
    Parts built before bash and the repo system are available aren't
    stored in a clean repository tarball, so if any early file is
    overwritten, it's lost. Fix this by creating a base.tar.bz2 right
    after the repo is set up, to hold reference copies of early files.
    This tarball isn't checksummed, since it varies considerably with
    bootstrap options, but the binaries inside are protected by their
    own checksums.

--- steps/improve/setup_repo.sh
+++ steps/improve/setup_repo.sh
@@ -5,3 +5,5 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 #
 mkdir -p /external/repo
+
+tar -cf - --exclude='/external/repo/*' --exclude='/external/repo-preseeded/*' --exclude='/external/distfiles/*' --exclude='/dev/*' --exclude='/proc/*' --exclude='/sys/*' --exclude='/tmp/*' / | bzip2 --best > /external/repo/base.tar.bz2
