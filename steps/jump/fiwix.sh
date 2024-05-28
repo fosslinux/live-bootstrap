e2d4782cc2d8e1e4718069b839952b79fbf57862
fosslinux <fosslinux@aussies.space>
2024-05-28 21:54:21 +1000

    Reclaim 5MB to the Fiwix rootfs from kexec
    We need just a couple more MB for configurator to be added!
    The tolerances are *very* tight now...

--- steps/jump/fiwix.sh
+++ steps/jump/fiwix.sh
@@ -7,11 +7,14 @@
 set -ex
 
 # Build the ext2 image
-make_fiwix_initrd -s 1376256 /boot/fiwix.ext2
+# 1392640 = 1360 MB
+make_fiwix_initrd -s 1381376 /boot/fiwix.ext2
 
 # Boot Fiwix
+# 199680 = 195 MB
+# as of 2024-05-27, Initrd = ~183 MB, kernel = ~10.5MB for Linux
 if match x${BARE_METAL} xTrue; then
-    kexec-fiwix /boot/fiwix -i /boot/fiwix.ext2 -m /e820 -c "fiwix console=/dev/tty1 root=/dev/ram0 initrd=fiwix.ext2 kexec_proto=linux kexec_size=204800 kexec_cmdline=\"init=/init consoleblank=0\""
+    kexec-fiwix /boot/fiwix -i /boot/fiwix.ext2 -m /e820 -c "fiwix console=/dev/tty1 root=/dev/ram0 initrd=fiwix.ext2 kexec_proto=linux kexec_size=199680 kexec_cmdline=\"init=/init consoleblank=0\""
 else
-    kexec-fiwix /boot/fiwix -i /boot/fiwix.ext2 -m /e820 -c "fiwix console=/dev/ttyS0 root=/dev/ram0 initrd=fiwix.ext2 kexec_proto=linux kexec_size=204800 kexec_cmdline=\"init=/init console=ttyS0\""
+    kexec-fiwix /boot/fiwix -i /boot/fiwix.ext2 -m /e820 -c "fiwix console=/dev/ttyS0 root=/dev/ram0 initrd=fiwix.ext2 kexec_proto=linux kexec_size=199680 kexec_cmdline=\"init=/init console=ttyS0\""
 fi
