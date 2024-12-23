473f75ffd99ce17d9f6a156c17ba354180a4ae1f
fosslinux <fosslinux@aussies.space>
2024-12-24 09:52:54 +1100

    Add autoconf symlinks to every autoconf pkg
    Early versions of automake have `autoconf` hardcoded into them as a
    program that should exist. We *were* "fixing" this by creating a symlink
    in autoconf-2.54. However this symlink is _not_ in the repo package,
    which broke some things. Also meant that from autoconf 2.54 through
    autoconf 2.71, automake was using autoconf 2.53.
    
    Let's make it consistent by having autoconf symlinks in every autoconf
    package.

--- steps/autoconf-2.71/pass1.sh
+++ steps/autoconf-2.71/pass1.sh
@@ -21,8 +21,8 @@ src_compile() {
 src_install() {
     make install MAKEINFO=true DESTDIR="${DESTDIR}"
 
-    ln -sf "${PREFIX}/bin/autoconf-2.71" "${DESTDIR}${PREFIX}/bin/autoconf"
-    ln -sf "${PREFIX}/bin/autoheader-2.71" "${DESTDIR}${PREFIX}/bin/autoheader"
-    ln -sf "${PREFIX}/bin/autom4te-2.71" "${DESTDIR}${PREFIX}/bin/autom4te"
-    ln -sf "${PREFIX}/bin/autoreconf-2.71" "${DESTDIR}${PREFIX}/bin/autoreconf"
+    ln -s autoconf-2.71 "${DESTDIR}${PREFIX}/bin/autoconf"
+    ln -s autoheader-2.71 "${DESTDIR}${PREFIX}/bin/autoheader"
+    ln -s autom4te-2.71 "${DESTDIR}${PREFIX}/bin/autom4te"
+    ln -s autoreconf-2.71 "${DESTDIR}${PREFIX}/bin/autoreconf"
 }
