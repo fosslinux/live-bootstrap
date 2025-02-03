838051151c6610e86383d7d4b23370c1dd4c0634
fosslinux <fosslinux@aussies.space>
2025-02-03 15:55:04 +1100

    Regenerate musl 1.1 using musl-chartable-tools

--- steps/musl-1.1.24/pass3.sh
+++ steps/musl-1.1.24/pass3.sh
@@ -5,6 +5,18 @@
 src_prepare() {
     default
 
+    # regenerate headers in src/ctype and src/iconv
+    cd ../musl-chartable-tools-44d780e03e78efcb3168ceab068170206dc92e85
+    patch -Np1 -i ../../files/cut.patch
+    cd ctype
+    CC=tcc make
+    cp alpha.h punct.h nonspacing.h wide.h ../../musl-1.1.24/src/ctype/
+    cd ../iconv
+    CC=tcc make
+    cp legacychars.h codepages.h jis0208.h gb18030.h hkscs.h ksc.h revjis.h \
+        ../../musl-1.1.24/src/locale/
+    cd ../../musl-1.1.24
+
     # tcc does not support complex types
     rm -rf src/complex
 
