f1416cf6ae30ab54ec49f94751d2d210c10c6f52
fosslinux <fosslinux@aussies.space>
2025-07-13 10:33:11 +1000

    Update dist, move earlier

--- steps/dist-3.5/pass1.sh
+++ steps/dist-3.5/pass1.sh
@@ -8,7 +8,9 @@
 src_prepare() {
     default
 
-    sed 's/@PERLVER@/5.10.1/' config.sh.in > config.sh
+    sed 's/@PERLVER@/5.6.2/' config.sh.in > config.sh
+    find . -name Makefile.SH -delete
+    rm -f Configure
 }
 
 src_compile() {
@@ -23,8 +25,8 @@ src_compile() {
 }
 
 src_install() {
-    mkdir -p "${DESTDIR}${PREFIX}/bin/" "${DESTDIR}${PREFIX}/lib/perl5/5.10.1"
+    mkdir -p "${DESTDIR}${PREFIX}/bin/" "${DESTDIR}${PREFIX}/lib/perl5/5.6.2"
     install mcon/metaconfig "${DESTDIR}${PREFIX}/bin/"
     install kit/manifake "${DESTDIR}${PREFIX}/bin/"
-    cp -r mcon/U/ "${DESTDIR}${PREFIX}/lib/perl5/5.10.1/"
+    cp -r mcon/U/ "${DESTDIR}${PREFIX}/lib/perl5/5.6.2/"
 }
