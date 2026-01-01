e4689a9467f3a6f43a564b31831e91ec83233961
Samuel Tyler <samuel@samuelt.me>
2026-01-01 17:01:53 +1100

    Update gperf

--- steps/gperf-3.3/pass1.sh
+++ steps/gperf-3.3/pass1.sh
@@ -3,15 +3,17 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 src_prepare() {
-    find . -name '*.info*' -delete -exec touch {} +
+    default
 
-    for d in doc tests lib src; do
-        cd $d
-        ln -s ../aclocal.m4 aclocal.m4
-        autoreconf-2.71 -fi
-        cd ..
-    done
-    autoreconf-2.71 -fi
+    rm doc/gperf.{1,dvi,info,pdf,ps} doc/*.html
+    touch doc/gperf.info doc/gperf.pdf
+
+    # Useless tests things
+    rm tests/*.exp tests/{languages,charsets}.gperf tests/lang-ucs2.in
+
+    GNULIB_SRCDIR=$(realpath ../gnulib-b08ee1d) \
+        AUTOCONF=autoconf-2.71 \
+        ./autogen.sh
 }
 
 src_configure() {
