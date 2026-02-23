156cd660c51470dacb7f15b740ad18b6e9e8f9d9
vxtls <187420201+vxtls@users.noreply.github.com>
2026-02-22 22:59:35 -0500

    fix(steps-guix): remove forced ext3 rootfstype in linux64 handover

--- steps-guix/jump/linux64.sh
+++ steps-guix/jump/linux64.sh
@@ -24,9 +24,9 @@ sync
 mkdir -p /etc
 if [ "${BARE_METAL}" = True ]; then
     kexec -l "${current_kernel}" \
-        --append="root=/dev/sda1 rootfstype=ext3 init=/init rw rootwait"
+        --append="root=/dev/sda1 init=/init rw rootwait"
 else
     kexec -l "${current_kernel}" --console-serial \
-        --append="console=ttyS0 root=/dev/sda1 rootfstype=ext3 init=/init rw rootwait"
+        --append="console=ttyS0 root=/dev/sda1 init=/init rw rootwait"
 fi
 kexec -e
