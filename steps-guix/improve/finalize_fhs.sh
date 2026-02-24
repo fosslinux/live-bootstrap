3bea5b5ce6a0706037c3caa0a4432de8e5d81a66
vxtls <187420201+vxtls@users.noreply.github.com>
2026-02-24 11:53:39 -0500

    fix(steps-guix): initialize /proc and mtab before delegating finalize_fhs

--- steps-guix/improve/finalize_fhs.sh
+++ steps-guix/improve/finalize_fhs.sh
@@ -2,4 +2,10 @@
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
 
+mkdir -p /proc /etc
+if [ ! -r /proc/mounts ]; then
+    mount -t proc proc /proc
+fi
+ln -snf /proc/mounts /etc/mtab
+
 exec /steps/improve/finalize_fhs.sh
