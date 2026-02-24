636edebab145c192ec450bcb1c6a46d65411f05c
vxtls <187420201+vxtls@users.noreply.github.com>
2026-02-23 20:11:36 -0500

    fix(steps-guix): make guile-gcrypt use statically derived PREFIX/LIBDIR for libgcrypt lookup

--- steps-guix/guile-gcrypt-0.5.0/pass1.sh
+++ steps-guix/guile-gcrypt-0.5.0/pass1.sh
@@ -6,24 +6,26 @@ src_prepare() {
 }
 
 src_configure() {
-    local host_triplet pkg_config_path libgcrypt_prefix libgcrypt_libdir
+    local host_triplet pkg_config_path
     host_triplet="$(gcc -dumpmachine)"
-    pkg_config_path="${LIBDIR}/pkgconfig:${PREFIX}/lib/pkgconfig"
-    libgcrypt_prefix="$(PKG_CONFIG_PATH="${pkg_config_path}" pkg-config --variable=prefix libgcrypt)"
-    libgcrypt_libdir="$(PKG_CONFIG_PATH="${pkg_config_path}" pkg-config --variable=libdir libgcrypt)"
+    pkg_config_path="${LIBDIR}/pkgconfig"
 
     mkdir -p build
     cd build
 
+    test -x "${PREFIX}/bin/libgcrypt-config"
+
     PATH="${PREFIX}/bin:/usr/bin:/bin" \
+    LIBGCRYPT_CONFIG="${PREFIX}/bin/libgcrypt-config" \
+    PKG_CONFIG_LIBDIR="${pkg_config_path}" \
     PKG_CONFIG_PATH="${pkg_config_path}" \
-    LD_LIBRARY_PATH="${libgcrypt_libdir}:${LIBDIR}:${PREFIX}/lib:${LD_LIBRARY_PATH}" \
+    LD_LIBRARY_PATH="${LIBDIR}:${LD_LIBRARY_PATH}" \
     ../configure \
         --prefix="${PREFIX}" \
         --libdir="${LIBDIR}" \
         --host="${host_triplet}" \
         --build="${host_triplet}" \
-        --with-libgcrypt-prefix="${libgcrypt_prefix}"
+        --with-libgcrypt-prefix="${PREFIX}"
 }
 
 src_compile() {
