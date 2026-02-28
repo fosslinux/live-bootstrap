1c75638d4a91ad289f774c8d367d24f96a826e31
vxtls <187420201+vxtls@users.noreply.github.com>
2026-02-28 15:30:24 -0500

    fix(steps-guix): add gdbm-1.8.3 before avahi and enable avahi dbm support

--- steps-guix/avahi-0.8/pass1.sh
+++ steps-guix/avahi-0.8/pass1.sh
@@ -17,6 +17,7 @@ src_configure() {
         --host="${host_triplet}" \
         --build="${host_triplet}" \
         --with-distro=lfs \
+        --enable-dbm \
         --enable-static \
         --enable-shared \
         --disable-glib \
---
+++ steps-guix/gdbm-1.8.3/pass1.sh
@@ -0,0 +1,28 @@
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+src_prepare() {
+    default
+}
+
+src_configure() {
+    local host_triplet
+    host_triplet="$(gcc -dumpmachine)"
+
+    PATH="${PREFIX}/bin:/usr/bin:/bin" \
+    ./configure \
+        --prefix="${PREFIX}" \
+        --libdir="${LIBDIR}" \
+        --includedir="${PREFIX}/include" \
+        --host="${host_triplet}" \
+        --build="${host_triplet}" \
+        --enable-static \
+        --enable-shared
+}
+
+src_compile() {
+    default_src_compile
+}
+
+src_install() {
+    default_src_install
+}
