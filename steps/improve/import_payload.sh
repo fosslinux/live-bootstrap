1b01d2261237b5c9f7682b3ea3288886dcca8c79
vxtls <187420201+vxtls@users.noreply.github.com>
2026-03-01 16:00:13 -0500

    fix(payload-import): run import after bash+device-node setup and remove kaem-stage node management

--- steps/improve/import_payload.sh
+++ steps/improve/import_payload.sh
@@ -5,14 +5,7 @@
 
 set -ex
 
-if match x${PAYLOAD_REQUIRED} xTrue; then
-    mkdir -p /dev
-    rm -f /dev/sda /dev/sdb /dev/sdc /dev/sdd
-    mknod -m 600 /dev/sda b 8 0
-    mknod -m 600 /dev/sdb b 8 16
-    mknod -m 600 /dev/sdc b 8 32
-    mknod -m 600 /dev/sdd b 8 48
-
+if [ "${PAYLOAD_REQUIRED}" = True ]; then
     mkdir -p /external/distfiles
     payload-import /external/distfiles
 fi
