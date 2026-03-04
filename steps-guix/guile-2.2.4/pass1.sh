81604307d9015dea0947876f819ec10ad317a7aa
vxtls <187420201+vxtls@users.noreply.github.com>
2026-03-04 16:55:47 -0500

    fix(steps-guix): add libffi build before guile-2.2.4 and wire explicit libffi pkg-config flags

--- steps-guix/guile-2.2.4/pass1.sh
+++ steps-guix/guile-2.2.4/pass1.sh
@@ -7,6 +7,17 @@ src_prepare() {
 }
 
 src_configure() {
+    local pkg_config_path libffi_cflags libffi_libs
+    pkg_config_path="${LIBDIR}/pkgconfig:${PREFIX}/lib/pkgconfig"
+    libffi_cflags="$(PKG_CONFIG_LIBDIR="${pkg_config_path}" PKG_CONFIG_PATH="${pkg_config_path}" \
+        pkg-config --cflags libffi)"
+    libffi_libs="$(PKG_CONFIG_LIBDIR="${pkg_config_path}" PKG_CONFIG_PATH="${pkg_config_path}" \
+        pkg-config --static --libs libffi)"
+
+    PKG_CONFIG_LIBDIR="${pkg_config_path}" \
+    PKG_CONFIG_PATH="${pkg_config_path}" \
+    LIBFFI_CFLAGS="${libffi_cflags}" \
+    LIBFFI_LIBS="${libffi_libs}" \
     ./configure \
         --prefix="${SEED_PREFIX}" \
         --disable-shared \
