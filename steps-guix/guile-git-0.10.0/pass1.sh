9055660d81cd857dd4f5ac43dddf9b46fb0b5d13
vxtls <187420201+vxtls@users.noreply.github.com>
2026-03-05 15:53:55 -0500

    fix(guile-git): link against static libgit2 explicitly

--- steps-guix/guile-git-0.10.0/pass1.sh
+++ steps-guix/guile-git-0.10.0/pass1.sh
@@ -6,14 +6,25 @@ src_prepare() {
 }
 
 src_configure() {
-    local host_triplet pkg_config_path
+    local host_triplet pkg_config_path libgit2_cflags libgit2_static_libs
     host_triplet="$(gcc -dumpmachine)"
-    pkg_config_path="${LIBDIR}/pkgconfig"
+    pkg_config_path="${LIBDIR}/pkgconfig:${PREFIX}/lib/pkgconfig:${PREFIX}/share/pkgconfig"
+    libgit2_cflags="$(PKG_CONFIG_LIBDIR="${pkg_config_path}" PKG_CONFIG_PATH="${pkg_config_path}" \
+        /usr/bin/pkg-config --cflags libgit2)"
+    libgit2_static_libs="$(PKG_CONFIG_LIBDIR="${pkg_config_path}" PKG_CONFIG_PATH="${pkg_config_path}" \
+        /usr/bin/pkg-config --static --libs libgit2)"
 
     PATH="${PREFIX}/bin:/usr/bin:/bin" \
-    PKG_CONFIG_LIBDIR="${pkg_config_path}:${PREFIX}/lib/pkgconfig" \
-    PKG_CONFIG_PATH="${pkg_config_path}:${PREFIX}/lib/pkgconfig" \
-    LD_LIBRARY_PATH="${LIBDIR}:${LD_LIBRARY_PATH}" \
+    PKG_CONFIG="/usr/bin/pkg-config" \
+    PKG_CONFIG_LIBDIR="${pkg_config_path}" \
+    PKG_CONFIG_PATH="${pkg_config_path}" \
+    CPPFLAGS="${libgit2_cflags} ${CPPFLAGS:-}" \
+    LIBS="${libgit2_static_libs}" \
+    LIBGIT2_CFLAGS="${libgit2_cflags}" \
+    LIBGIT2_LIBS="${libgit2_static_libs}" \
+    libgit2_CFLAGS="${libgit2_cflags}" \
+    libgit2_LIBS="${libgit2_static_libs}" \
+    LD_LIBRARY_PATH="${LIBDIR}:${PREFIX}/lib:${LD_LIBRARY_PATH}" \
     ./configure \
         --prefix="${PREFIX}" \
         --libdir="${LIBDIR}" \
@@ -22,6 +33,20 @@ src_configure() {
 }
 
 src_compile() {
+    local pkg_config_path libgit2_cflags libgit2_static_libs
+
+    pkg_config_path="${LIBDIR}/pkgconfig:${PREFIX}/lib/pkgconfig:${PREFIX}/share/pkgconfig"
+    libgit2_cflags="$(PKG_CONFIG_LIBDIR="${pkg_config_path}" PKG_CONFIG_PATH="${pkg_config_path}" \
+        /usr/bin/pkg-config --cflags libgit2)"
+    libgit2_static_libs="$(PKG_CONFIG_LIBDIR="${pkg_config_path}" PKG_CONFIG_PATH="${pkg_config_path}" \
+        /usr/bin/pkg-config --static --libs libgit2)"
+
+    CPPFLAGS="${libgit2_cflags} ${CPPFLAGS:-}" \
+    LIBS="${libgit2_static_libs}" \
+    LIBGIT2_CFLAGS="${libgit2_cflags}" \
+    LIBGIT2_LIBS="${libgit2_static_libs}" \
+    libgit2_CFLAGS="${libgit2_cflags}" \
+    libgit2_LIBS="${libgit2_static_libs}" \
     default_src_compile
 }
 
