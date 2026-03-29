00fe9753e46eb86db6381227faa1713be53bffa3
vxtls <187420201+vxtls@users.noreply.github.com>
2026-03-29 11:07:09 -0400

    fix: autoreconf -fi in guile-2.0.9 to regen build files

--- steps-guix/guile-2.0.9/pass1.sh
+++ steps-guix/guile-2.0.9/pass1.sh
@@ -5,7 +5,9 @@ SEED_PREFIX="/bootstrap-seeds/guile-2.0.9"
 src_prepare() {
     default
 
-    AUTOPOINT=true autoreconf -fi
+    AUTOPOINT=true \
+    ACLOCAL_PATH="${PREFIX}/share/aclocal:${PREFIX}/share/gettext/m4${ACLOCAL_PATH:+:${ACLOCAL_PATH}}" \
+    autoreconf -fi
 
     # Match Guix's static bootstrap Guile approach: force the final `guile`
     # executable to be linked through libtool with -all-static.
