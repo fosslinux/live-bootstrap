163bfbeeffd9825af213535fd22167ec541573e6
vxtls <187420201+vxtls@users.noreply.github.com>
2026-04-08 08:09:03 -0400

    fix: remove iso_store_path usage, just for test

--- steps-guix/improve/guix-build-iso.sh
+++ steps-guix/improve/guix-build-iso.sh
@@ -21,16 +21,16 @@ mkdir -p "${out_dir}"
 
 cp /var/guix/profiles/per-user/root/current-guix/manifest /usr/manifest
 
-iso_store_path="$(guix system image \
+guix system image \
     --system=x86_64-linux \
     -t iso9660 \
     -c "${JOBS}" \
     -e '(@@ (gnu system install) installation-os)' \
-    --no-substitutes)"
+    --no-substitutes
 
-if [ ! -e "${iso_store_path}" ]; then
-    echo "guix system image did not return a valid path: ${iso_store_path}" >&2
-    exit 1
-fi
+# if [ ! -e "${iso_store_path}" ]; then
+#     echo "guix system image did not return a valid path: ${iso_store_path}" >&2
+#     exit 1
+# fi
 
-ln -sfn "${iso_store_path}" "${out_dir}/guix-system-install-x86_64.iso"
+# ln -sfn "${iso_store_path}" "${out_dir}/guix-system-install-x86_64.iso"
