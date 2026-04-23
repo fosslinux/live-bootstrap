c37ee0c57a59b4c863b88ad35fd35fda2004258f
Haelwenn (lanodan) Monnier <contact@hacktivis.me>
2026-04-23 03:00:19 +0200

    utils-std: upgrade to 0.2.0
    Changelog at: https://distfiles.hacktivis.me/releases/utils-std/utils-std-0.2.0.txt
    Notably adds comm(1) command and `ln -r`
    
    For the syscalls not supported on Fiwix it would probably be better
    to remove them from pre-linux-kexec musl as otherwise it means
    broken compile+link ./configure tests as we can see here.

--- steps/utils-std-0.2.0/pass1.sh
+++ steps/utils-std-0.2.0/pass1.sh
@@ -15,4 +15,12 @@ src_prepare() {
 src_configure() {
 	chmod +x configure
 	./configure PREFIX="${PREFIX}" CC=tcc AR=tcc\ -ar
+
+	# Fiwix (as of 1.7.0) doesn't have sendfile(2), copy_file_range(2), syncfs(2), posix_fadvise(2)
+	sed -i \
+		-e /HAS_SENDFILE/d \
+		-e /HAS_COPY_FILE_RANGE/d \
+		-e /HAS_SYNCFS/d \
+		-e /HAS_POSIX_FADVISE/d \
+		config.h
 }
